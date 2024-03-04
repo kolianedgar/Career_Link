98fa2e1a7c34992142e02a8144b3310b879fcff2
kolianedgar <149166972+kolianedgar@users.noreply.github.com>
2024-03-04 15:05:08 +0100

    Starting the implementation of logout process and combining register/login with chat.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.Network;
 import android.net.NetworkRequest;
@@ -15,6 +17,7 @@ import android.text.style.StyleSpan;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -49,7 +52,8 @@ public class MainActivity extends AppCompatActivity {
     private Queue<Message> messageQueue = new LinkedList<>();
     private boolean isConnected = true;
     private FirebaseAuth mAuth;
-
+    private ImageButton logout_btn;
+    private SharedPreferences sharedPreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -199,6 +203,20 @@ public class MainActivity extends AppCompatActivity {
                 });
 
             }
+        });
+
+        logout_btn = findViewById(R.id.logout_button);
+        mAuth = FirebaseAuth.getInstance();
+        sharedPreferences = getSharedPreferences("autoLogin", Context.MODE_PRIVATE);
+
+        logout_btn.setOnClickListener(v -> {
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putInt("key", 0);
+            editor.apply();
+
+            mAuth.signOut();
+            Intent redirect_login = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(redirect_login);
         });
     }
 
